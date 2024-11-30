1a4f23f68db8595c0bcbc06cdc753741f23702ea
Francesco Andreotti <francesco.andreotti@mail.polimi.it>
2024-11-30 20:06:22 +0100

    modificato src/EulerianSprayProblem.cpp e .gitignore per togliere le cose di VSCode

--- src/EulerianSprayProblem.cpp
+++ src/EulerianSprayProblem.cpp
@@ -1,7 +1,16 @@
-#include"EulerianSpray"
+#include"EulerianSprayProblem.h"
 
 template <int dim>
-void EulerianSprayProblem::run{
+void EulerianSprayProblem::make_grid_and_dofs(){
+    // In step 67 this is a global variable. I may opt for a solution like Felotti's one, which uses a parameter memeber and make it parameters.testcase
+    switch(testcase){
+        case 1:
+            
+    }
+}
+
+template <int dim>
+void EulerianSprayProblem::run(){
     //Qua nello step 67 c'è un pezzetto per quando si usa MPI
 
     make_grid_and_dofs();
