4b9e33b49341f1cc47126575f7287bd893c42aef
Francesco Andreotti <francesco.andreotti@mail.polimi.it>
2026-01-15 10:02:26 +0100

    Just changed the name of the variable velocity_norm to momentum_norm, which is more correct

--- inc/FindIntersection_IMP.hpp
+++ inc/FindIntersection_IMP.hpp
@@ -98,10 +98,10 @@ find_intersection_1d_LLF_Variant(const dealii::Tensor<1, dim, myReal> & q, const
   // True if I am below the line rho * a + \epsilon
   bool below = (q[1] < q[0] * a + epsilon);
 
-  // If the solution is already in G_\epsilon theta is = 1
-  if (!above && !below)
+  
+  if (!above && !below)// If the solution is already in G_\epsilon theta is = 1
     return q;
-  if (above && below)
+  if (above && below) //If the solution is in the small triangle is not in G_\epsilon but still in G
     return q;
 
   double edge = above ? b : a ;
@@ -112,7 +112,7 @@ find_intersection_1d_LLF_Variant(const dealii::Tensor<1, dim, myReal> & q, const
     // const myReal m   = w[1] + t*(q[1] - w[1]);
     const myReal rho = (1.-t) * w[0] + t * q[0];
     const myReal m = (1.-t) * w[1] + t * q[1];
-    return static_cast<double>(m - rho * edge TODO:epsilon );
+    return static_cast<double>(m - rho * edge + (1-2*static_cast<double>(above))*epsilon);
   };
 
   double t0 = 0.0, t1 = 1.0;
@@ -176,24 +176,24 @@ template<int dim> dealii::Tensor<1, dim, myReal> find_intersection(
   double epsilon,
   double S /* max velocity norm*/)
 { 
-  // I put this piece in a scope to hide the name velocity_norm and use it in
+  // I put this piece in a scope to hide the name momentum_norm and use it in
   // the lambda function
   {
     double frontier = (S + epsilon)*(S + epsilon)*q[0]*q[0];
-    double velocity_norm = 0;
+    double momentum_norm = 0;
     for(unsigned d = 1; d < dim; ++d)
-      velocity_norm += (q[d]*q[d]);
-    if( velocity_norm <= frontier)
+      momentum_norm += (q[d]*q[d]);
+    if( momentum_norm <= frontier)
       return q;
   }
   auto f = [&](double t)->double
   {
     const auto s = (1.-t) * w + t * q;
-    double velocity_norm = 0;
+    double momentum_norm = 0;
     for(unsigned d = 1; d < dim; ++d)
-      velocity_norm += (s[d]*s[d]);
+      momentum_norm += (s[d]*s[d]);
 
-    return velocity_norm - (S + epsilon)*(S + epsilon)*s[0]*s[0];
+    return momentum_norm - (S + epsilon)*(S + epsilon)*s[0]*s[0];
   };
 
   double t0 = 0., t1 = 1.;
@@ -202,10 +202,10 @@ template<int dim> dealii::Tensor<1, dim, myReal> find_intersection(
   const int max_it = 100;
   const double tol = 1e-14;
   double tm, fm;
-
-  if(f0 * f1 > 0.) //if both actual velocity and mean velocity are outside the admissibility region
-    return w; // i choose s as the average solution
-  else // otherwise I go on with bisection
+  Assert(f0 * f1 > 0., ExcMessage("Problem in find_intersection: solution average appears to be outside G region"))
+  // if(f0 * f1 > 0.) //if both actual velocity and mean velocity are outside the admissibility region
+  //   return w; // i choose s as the average solution
+ // else // otherwise I go on with bisection
   {
     for(int it = 0; it < max_it; ++it)
     {
